2c4419e789ada209c792f8c749f8ef4d6ac2e4b6
ximbest <vitalik.nayda@gmail.com>
2021-01-15 10:13:29 +0200

    add 8-9 fixed connection strings

--- 8-9/eventHub.scala
+++ 8-9/eventHub.scala
@@ -2,15 +2,13 @@ import org.apache.spark.eventhubs.{ ConnectionStringBuilder, EventHubsConf, Even
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 
-// To connect to an Event Hub, EntityPath is required as part of the connection string.
-// Here, we assume that the connection string from the Azure portal does not have the EntityPath part.
-val appID = "ea07640e-c333-4be8-9bc3-d817f30d5fcb"
-val password = "r01iYou6G_O8GiU7II-E-4BOqp2-ei6VeX"
+val appID = "be4c1d34-ac5f-45bf-bd26-216f8a4273f8"
+val password = "FFo3uHZx5QxOFF4~~JJ9I4_zO5j.NM.2If"
 val tenantID = "7631cd62-5187-4e15-8b8e-ef653e366e7a"
-val fileSystemName = "naidacontainer"
-val storageAccountName = "naidaaccount"
-val connectionString = ConnectionStringBuilder("Endpoint=sb://naidaspace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=CLG2Pg719qKRTuFohSZF+kHagFUnxJeafNujjdWOj0k=")
-  .setEventHubName("naidavitalii")
+val fileSystemName = "naida9"
+val storageAccountName = "naida9"
+val connectionString = ConnectionStringBuilder("Endpoint=sb://naida9.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XfUW/+PEO2s+RFOa4Dqg7rsktAQ8DTnsFTWeBd6WFb4=")
+  .setEventHubName("naida9")
   .build
 val eventHubsConf = EventHubsConf(connectionString)
   .setStartingPosition(EventPosition.fromEndOfStream)
@@ -29,8 +27,9 @@ val filtered = streamingInputDF.select (
      , get_json_object(col("body").cast(StringType), "$.parameter_code").alias("parameter_code")
 )
 
+
 filtered.writeStream
   .format("com.databricks.spark.csv")
   .outputMode("append")
-  .option("checkpointLocation", "/mnt/naidacontainer/labnaida8")
-  .start("/mnt/naidacontainer/labnaida8")
+  .option("checkpointLocation", "/mnt/naida9/lab9dir")
+  .start("/mnt/naida9/lab9dir")
--- 8-9/mount.py
+++ 8-9/mount.py
@@ -1,16 +1,14 @@
-import dbutils
-
 configs = {"fs.azure.account.auth.type": "OAuth",
            "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-           "fs.azure.account.oauth2.client.id": "ea07640e-c333-4be8-9bc3-d817f30d5fcb",
-           "fs.azure.account.oauth2.client.secret": "r01iYou6G_O8GiU7II-E-4BOqp2-ei6VeX",
+           "fs.azure.account.oauth2.client.id": "be4c1d34-ac5f-45bf-bd26-216f8a4273f8",
+           "fs.azure.account.oauth2.client.secret": "FFo3uHZx5QxOFF4~~JJ9I4_zO5j.NM.2If",
            "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/7631cd62-5187-4e15-8b8e-ef653e366e7a/oauth2/token",
            "fs.azure.createRemoteFileSystemDuringInitialization": "true"}
 
-# Optionally, you can add <directory-name> to the source URI of your mount point.
 dbutils.fs.mount(
-  source = "abfss://naidacontainer@naidaaccount.dfs.core.windows.net/",
-  mount_point = "/mnt/naidacontainer",
+  source = "abfss://naida9@naida9.dfs.core.windows.net/",
+  mount_point = "/mnt/naida9/",
   extra_configs = configs)
 
-display(dbutils.fs.ls('/mnt/naidacontainer'))
+#для демонстрації заеманченої діру
+display(dbutils.fs.ls('/mnt/naida9'))
